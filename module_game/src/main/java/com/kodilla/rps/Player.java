595ec41507ac5c015d93896e2127b2e84421ae95
Patryk Rejent <82150867+Patryk244@users.noreply.github.com>
2025-08-14 11:29:00 +0200

    Remove not used methods from Player class

--- module_game/src/main/java/com/kodilla/rps/Player.java
+++ module_game/src/main/java/com/kodilla/rps/Player.java
@@ -12,16 +12,4 @@ public class Player {
         return name;
     }
 
-
-    public int getScore(){
-        return score;
-    }
-
-    public void setScore(int score){
-        this.score = score;
-    }
-
-    public void resetScore(){
-        this.score = 0;
-    }
 }
